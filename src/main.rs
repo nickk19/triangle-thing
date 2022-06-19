0385ac680e3ab9e9359ee2d2b48950e05ffbf8cf
nickk19 <nikko8204@gmail.com>
2022-06-19 15:30:01 +0200

    Removed rows number print

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,7 @@ fn main() {
 
     print!("How many rows do you want? ");
     read(&mut rows_num);
-
     let rows_num:u8 = rows_num.trim().parse().expect("Not a valid number");
-    println!("{} is the number of rows to print\n", rows_num);
 
     for _ in 1..=rows_num {
         println!("{}", character);
