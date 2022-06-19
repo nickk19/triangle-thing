9caf44f08a76aced41508eab1ebcb7e314130275
nickk19 <nikko8204@gmail.com>
2022-06-19 14:29:01 +0200

    Implemented basic iteration loop based on user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{self, Write};
+
+fn read (input: &mut String) {
+    let _ = io::stdout().flush();
+    io::stdin()
+        .read_line(input)
+        .expect("Failed to read from stdin");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rows_num = String::new();
+
+    print!("How many rows do you want? ");
+    read(&mut rows_num);
+
+    let rows_num:u8 = rows_num.trim().parse().expect("Not a valid number");
+    println!("{} is the number of rows to print\n", rows_num);
+
+    for i in 1..rows_num+1 {
+        println!("Iteration number {}", i);
+    }
+    
 }
